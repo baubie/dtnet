c75eaebf7d6e3862daa131d4d956cfc46ad30f1b
Brandon Aubie <brandon@aubie.ca>
2009-04-01 15:26:43 +0000

    Using Lazy Loading

--- src/libdtnet/neuronfactory.cpp
+++ src/libdtnet/neuronfactory.cpp
@@ -70,7 +70,7 @@ bool NeuronFactory::close() {
     std::map<Neuron*, std::string>::iterator n_iter;
 
     for (n_iter = this->models.begin(); n_iter != this->models.end(); ++n_iter) {
-        void* handle = dlopen(n_iter->second.c_str(), RTLD_NOW);
+        void* handle = dlopen(n_iter->second.c_str(), RTLD_LAZY);
         if (!handle) {
             std::cerr << "Cannot load library: " << dlerror() << std::endl;
             return false;
